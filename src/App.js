932a33f6c8631be04f5fe49a18cb0f30142317c4
Justin C Behringer <63168114+jbehringer95@users.noreply.github.com>
2022-01-25 23:04:24 -0600

    Updating App.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
+import { buildTimeValue } from "@testing-library/user-event/dist/utils";
 import React, { Component } from "react";
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = { displayBio: false };
+
+        console.log('Component this', this);
+
+        this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+    }
+
+    toggleDisplayBio() {
+        this.setState({ displayBio: !this.state.displayBio})
+    }
+
     render() {
        return (
            <div>
                <h1>Hello!</h1>
                <p>My name is Justin. I'm a Data Scientist.</p>
                <p>I'm always looking forward to working on meaningful projects.</p>
+               {
+                   this.state.displayBio ? (
+                    <div>
+                        <p>I live in Centralia, and I code pretty much everyday</p>
+                        <p>My favorite language is Python and I think machine learning models are amazing.</p>
+                        <p>Besides coding, I love music and watching football.</p>
+                        <button onClick={this.toggleDisplayBio}>Show less</button>
+                    </div>
+                    ) : (
+                        <div>
+                            <button onClick={this.toggleDisplayBio}>Read more</button>
+                        </div>
+                    )
+               }
            </div>
        ) 
     }
